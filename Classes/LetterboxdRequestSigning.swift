3b0874d8e8ab3242a0b67bb523e4dc4209c50910
Ryan Maxwell <ryanm@xwell.nz>
2017-07-03 22:41:37 +1200

    make NSMutableURLRequest extension public to framework.

--- Classes/LetterboxdRequestSigning.swift
+++ Classes/LetterboxdRequestSigning.swift
@@ -55,7 +55,7 @@ public extension URLRequest {
     }
 }
 
-extension NSMutableURLRequest {
+public extension NSMutableURLRequest {
     
     @objc(signedRequestForURL:HTTPMethod:HTTPBody:APIKey:APISecret:)
     class func signedRequest(for url: URL, httpMethod: String, httpBody: Data?, apiKey: String, apiSecret: String) -> NSMutableURLRequest {
